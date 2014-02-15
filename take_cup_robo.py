d5f6da9a85fb9e2ec079b6a5e1f51249f97c6557
Dan Haiduc <danuthaiduc@gmail.com>
2014-02-15 20:44:39 +0200

    modified take_cup_robo for new event register

--- take_cup_robo.py
+++ take_cup_robo.py
@@ -13,24 +13,24 @@ if __name__ == "__main__":
     r_c = RoboController()
     cd = CupDetector(e)
     fd = FaceDetector(e)
-    e.register(l_c, 'cup_appeared')  # Arm may align to grasp
-    e.register(l_c, 'cup_grasped')  # Arm may align to release
-    e.register(n_c, 'arm_aligned')  # Arm aligned for grasping
-    e.register(n_c, 'cup_over_tray')  # Arm aligned for releasing
-    e.register(l_c, 'cup_released')  # Arm may move to initial position
-    e.register(n_c, 'cup_released')  # Arm may move to initial position                                 
+    e.register('l_c', l_c, 'cup_appeared')  # Arm may align to grasp
+    e.register('l_c', l_c, 'cup_grasped')  # Arm may align to release
+    e.register('n_c', n_c, 'arm_aligned')  # Arm aligned for grasping
+    e.register('n_c', n_c, 'cup_over_tray')  # Arm aligned for releasing
+    e.register('l_c', l_c, 'cup_released')  # Arm may move to initial position
+    e.register('n_c', n_c, 'cup_released')  # Arm may move to initial position
 
-    
-    e.register(cd, 'frame')
-    # TODO: only use frames for face det after 
-    e.register(fd, 'frame')  # We can see!
-    
-    e.register(r_c, 'no_cup')  # When no cup, spin my head right round
-    e.register(r_c, 'face_gone')  # Face disappeared, wait for drink pickup
-    e.register(r_c, 'no_face')  # No face detected, try rotating
-    e.register(r_c, 'face_pos')  # Face detected
+    e.register('cd', cd, 'frame')
+    # TODO: only use frames for face det after
+    e.register('fd', fd, 'frame')  # We can see!
+
+    e.register('r_c', r_c, 'no_cup')  # When no cup, spin my head right round
+    e.register('r_c', r_c, 'face_gone')  # Face disappeared, wait for drink pickup
+    e.register('r_c', r_c, 'no_face')  # No face detected, try rotating
+    e.register('r_c', r_c, 'face_pos')  # Face detected
     e.register(Webcam(e, cam=1, cam_angle=-25))
     e.add_event('cup_released', True)
+
     try:
         e.run()
     except (KeyboardInterrupt, SystemExit):
