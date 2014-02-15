6980ae16ba9da0dadb61754282c6d790079dc138
Roland Szabo <rolisz@yahoo.com>
2014-02-15 16:54:25 +0200

    Refactor events

--- event.py
+++ event.py
@@ -1,5 +1,5 @@
 import Queue
-from threading import Thread
+from threading import Thread, Event
 
 
 class EventLoop(object):
@@ -8,6 +8,8 @@ class EventLoop(object):
         self.registry = {}
         self.threads = set()
         self.queue = Queue.Queue()
+        self.run_flag = Event()
+        self.run_flag.set()
 
     def register(self, thread_obj, event=None):
         """
@@ -31,18 +33,24 @@ class EventLoop(object):
         for thread in self.threads:
             thread.start()
         while True:
-            event, value = self.queue.get(True, 20000)
-            if event not in self.registry:
-                print("Invalid event encountered %s with values %r!" %
-                      (event, value))
-            for listener in self.registry[event]:
-                # print('call', listener, event, value)
-                listener(event, value)
+            try:
+                event, value = self.queue.get(True, 20000)
+                if event not in self.registry:
+                    print("Invalid event encountered %s with values %r!" %
+                          (event, value))
+                for listener in self.registry[event]:
+                    # print('call', listener, event, value)
+                    listener(event, value)
+            except KeyboardInterrupt:
+                self.run_flag.clear()
+                for thread in self.threads:
+                    thread.join()
 
 
 class EventEmitter(Thread):
     def __init__(self, ev):
         self.ev = ev
+        self.run_flag = ev.run_flag
         super(EventEmitter, self).__init__()
 
     def emit(self, event, values):
@@ -50,12 +58,19 @@ class EventEmitter(Thread):
 
 
 class EventConsumer(Thread):
-    def __init__(self):
+    def __init__(self, flag=None):
         self.queue = Queue.Queue()
+        if flag is not None:
+            self.run_flag = flag
         super(EventConsumer, self).__init__()
 
     def add_event(self, event, value):
         self.queue.put((event, value))
+
+    def run(self):
+        while self.run_flag.is_set():
+            event, value = self.queue.get(True, 20000)
+            getattr(self, event)(value)
 
 
 class DecisionMaker(EventEmitter, EventConsumer):
@@ -78,13 +93,15 @@ if __name__ == '__main__':
                 sleep(random())
 
     class Consumer(EventConsumer):
-        def __init__(self, i):
+        def __init__(self, flag, i):
             self.i = i
-            super(Consumer, self).__init__()
+            super(Consumer, self).__init__(flag)
 
-        def run(self):
-            for i in range(20):
-                print(self.i, self.queue.get(True))
+        def event1(self, value):
+            print('event1', value)
+
+        def event2(self, value):
+            print('event2', value)
 
     e = EventLoop()
 
@@ -92,8 +109,8 @@ if __name__ == '__main__':
     e.register(Producer(e, 'event1'))
     e.register(Producer(e, 'event2'))
 
-    e.register(Consumer('c1'), 'event1')
-    e.register(Consumer('c2'), 'event2')
-    e.register(Consumer('c3'), 'event1')
+    e.register(Consumer(e.run_flag, 'c1'), 'event1')
+    e.register(Consumer(e.run_flag, 'c2'), 'event2')
+    e.register(Consumer(e.run_flag, 'c3'), 'event1')
 
     e.run()
