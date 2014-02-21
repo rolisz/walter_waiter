07414e741331a3c8034315474d9d70c5f0cf13bf
titeipa <tpie1337@scs.ubbcluj.ro>
2014-02-21 18:30:29 +0200

    Create ultrasonic.py

---
+++ sensors/ultrasonic.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Ultrasonic(event.EventEmitter):
+    def __init__(self, ev):
+        self.serial_arduino = serial.Serial("COM9", 57600, timeout=1)
+        self.trayLength = 30 # in cm
+        self.history = deque() # history of reads
+        self.historyLength = 20 # history length
+        self.emitThreshold = 18 
+        super(Ultrasonic, self).__init__(ev)
+
+    def run(self):
+        while self.run_flag.is_set():
+            self.history.append(self.cupInTray())
+            if len(self.history) == self.historyLength + 1:
+                self.history.popleft()
+                if self.history.count(False) > self.emitThreshold:
+                    self.emit('no_cups_on_tray')
+        
+    def cupInTray():
+        duration = ''
+        while duration == '': # sometimes a null read is made, make another one
+            duration = super.serial_arduino.readline().strip() # read duration in ms for sound to travel to first object and back    
+        cm = int(duration) / 29 / 2 # convert to cm
+        if cm < self.trayLength:
+            return True
+        return False
