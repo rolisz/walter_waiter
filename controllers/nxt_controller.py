87126e464aa6b3505b4510cfb94f09cf5dfbfdd1
Țiței Paul Adrian <titei.pa@gmail.com>
2014-02-27 14:31:45 +0200

    Added lego ultrasound sensor

--- controllers/nxt_controller.py
+++ controllers/nxt_controller.py
@@ -1,6 +1,7 @@
 import event
 import nxt.locator
 import nxt.motor as motor
+import nxt.sensor as sensor
 
 
 
@@ -9,7 +10,7 @@ class NxtController(event.DecisionMaker):
         self.brick = nxt.locator.find_one_brick()
         self.motor = motor.Motor(self.brick, motor.PORT_B)
         self.height_motor = motor.Motor(self.brick, motor.PORT_A)
-        self.obstacle_detector = None # #todo also issue on how to listen to both event loop and to this
+        self.obstacle_detector = sensor.Ultrasonic(self.brick, sensor.PORT_1)
         super(NxtController, self).__init__(ev)
 
     def run(self):
@@ -36,3 +37,7 @@ class NxtController(event.DecisionMaker):
         except motor.BlockedException:
             self.motor.idle()
         self.emit('cup_released', True)
+
+    def obstacle_distance(self, _):
+        distance = self.obstacle_detector.get_distance()
+        self.emit('obstacle', distance)
